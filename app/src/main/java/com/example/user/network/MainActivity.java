cb08c797c1a6f127129e5bb6d4d9f1001476d6ce
walkermannum1 <2469377884@qq.com>
2016-06-16 16:37:41 +0800

    Network

--- app/src/main/java/com/example/user/network/MainActivity.java
+++ app/src/main/java/com/example/user/network/MainActivity.java
@@ -1,13 +1,79 @@
 package com.example.user.network;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.webkit.HttpAuthHandler;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    public static final int SHOW_RESPOSE = 0;
+    private Button sendRequest;
+    private TextView responseText;
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SHOW_RESPOSE:
+                    String response = (String) msg.obj;//handle UI,show response
+                    responseText.setText(response);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sendRequest = (Button) findViewById(R.id.send_request);
+        responseText = (TextView) findViewById(R.id.response);
+        sendRequest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.send_request) {
+            sendRequestWithHttpURLConnection();
+        }
+    }
+    private void sendRequestWithHttpURLConnection() {
+        //Start a thread to lanch web request
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL("http://www.baidu.com");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine())!= null) {
+                        response.append(line);
+                    }
+                    Message message = new Message();
+                    message.what = SHOW_RESPOSE;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
     }
 }
