83304cc5c5827576d95f950bc2c0da84e32b1246
walkermannum1 <2469377884@qq.com>
2016-06-20 17:24:19 +0800

    Plus

--- app/src/main/java/com/example/user/network/HttpUtil.java
+++ app/src/main/java/com/example/user/network/HttpUtil.java
@@ -1,8 +1,6 @@
 package com.example.user.network;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
